4141ef7f29cf246df8709764deabdd3a1e23a8f3
DB <2502523450@qq.com>
2025-01-11 19:05:30 +0800

    feat: cucumber示例

---
+++ src/test/java/com/rockblade/CucumberRunner.java
@@ -0,0 +1,18 @@
+package com.rockblade;
+
+import org.junit.platform.suite.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+/**
+ * Cucumber测试运行器
+ * 集成Spring Boot测试环境
+ */
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.rockblade.steps")
+@SpringBootTest
+public class CucumberRunner {
+} 
