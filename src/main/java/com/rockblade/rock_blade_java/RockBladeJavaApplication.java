33519d920a3ad7c3933085fd990bbb3acb93d423
DB <2502523450@qq.com>
2025-01-11 18:28:52 +0800

    feat: 项目初始化

---
+++ src/main/java/com/rockblade/rock_blade_java/RockBladeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rockblade.rock_blade_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockBladeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockBladeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockblade/rock_blade_java/RockBladeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockblade.rock_blade_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockBladeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
