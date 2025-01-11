aaa216d3c7ae7cb15ab4224abe9fd90fc2a00087
DB <2502523450@qq.com>
2025-01-11 19:05:11 +0800

    feat: 测试config类与helper类

---
+++ src/test/java/com/rockblade/config/TestConfig.java
@@ -0,0 +1,11 @@
+package com.rockblade.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+/**
+ * 测试配置类
+ */
+@TestConfiguration
+public class TestConfig {
+    // 在这里添加测试专用的bean配置
+}
