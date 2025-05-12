2dfd737639d3f635fadc4e1e2afc01455c93c6b9
DB <2502523450@qq.com>
2025-05-12 18:54:47 +0800

    🔥feat: 新增角色权限管理模块，包括实体、服务、控制器及映射文件

---
+++ src/main/java/com/rockblade/domain/system/service/impl/RolePermissionServiceImpl.java
@@ -0,0 +1,18 @@
+package com.rockblade.domain.system.service.impl;
+
+import com.mybatisflex.spring.service.impl.ServiceImpl;
+import com.rockblade.domain.system.entity.RolePermission;
+import com.rockblade.infrastructure.mapper.RolePermissionMapper;
+import com.rockblade.domain.system.service.RolePermissionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 角色和菜单关联表 服务层实现。
+ *
+ * @author 
+ * @since 2025-05-12
+ */
+@Service("rolePermissionService")
+public class RolePermissionServiceImpl extends ServiceImpl<RolePermissionMapper, RolePermission> implements RolePermissionService {
+
+}
