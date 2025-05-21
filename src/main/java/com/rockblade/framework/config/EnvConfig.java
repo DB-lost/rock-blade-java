00d84bff47ed4e4b697cf8e63f573b53516af3b5
DB <2502523450@qq.com>
2025-05-21 11:27:18 +0800

    ✨feat: 实现.env文件的加载功能，添加磁盘和网络监控相关消息的国际化支持

--- src/main/java/com/rockblade/framework/config/EnvConfig.java
+++ src/main/java/com/rockblade/framework/config/EnvConfig.java
@@ -2,8 +2,8 @@
  * @Author: DB 2502523450@qq.com
  * @Date: 2025-02-22 23:18:25
  * @LastEditors: DB 2502523450@qq.com
- * @LastEditTime: 2025-02-23 09:16:39
- * @FilePath: /rock-blade-admin-java/src/main/java/com/rockblade/framework/config/EnvConfig.java
+ * @LastEditTime: 2025-05-21 11:24:56
+ * @FilePath: /rock-blade-java/src/main/java/com/rockblade/framework/config/EnvConfig.java
  * @Description: 读取.env文件的配置
  *
  * Copyright (c) 2025 by RockBlade, All Rights Reserved.
@@ -12,31 +12,33 @@ package com.rockblade.framework.config;
 
 import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.stereotype.Component;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
 
-@Configuration
-public class EnvConfig {
+@Component
+public class EnvConfig implements EnvironmentPostProcessor {
 
-  @Autowired private ConfigurableEnvironment environment;
+  @Override
+  public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+    try {
+      // 加载.env文件
+      Dotenv dotenv = Dotenv.configure().directory(System.getProperty("user.dir")).filename(".env").load();
 
-  @PostConstruct
-  public void init() {
-    // 加载.env文件
-    Dotenv dotenv =
-        Dotenv.configure().directory(System.getProperty("user.dir")).filename(".env").load();
+      // 将.env的配置转换为Properties
+      Properties properties = new Properties();
+      dotenv.entries().forEach(entry -> properties.put(entry.getKey(), entry.getValue()));
 
-    // 将.env的配置转换为Properties
-    Properties properties = new Properties();
-    dotenv.entries().forEach(entry -> properties.put(entry.getKey(), entry.getValue()));
-
-    // 将Properties添加到Spring环境中，优先级设置为最高
-    PropertiesPropertySource propertySource = new PropertiesPropertySource("dotenv", properties);
-    environment.getPropertySources().addFirst(propertySource);
+      // 将Properties添加到Spring环境中，优先级设置为最高
+      PropertiesPropertySource propertySource = new PropertiesPropertySource("dotenv", properties);
+      environment.getPropertySources().addFirst(propertySource);
+    } catch (Exception e) {
+      // 记录错误但允许应用继续启动
+      System.err.println("Failed to load .env file: " + e.getMessage());
+    }
   }
 }
